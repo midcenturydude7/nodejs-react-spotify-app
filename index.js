289f4460052cd7bc3c1e3b2399236b39047b9470
midCenturyDude7 <mgriffes@gmail.com>
2022-04-07 17:04:59 -0700

    Working to establish connection with Spotify API

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
+require('dotenv').config()
 const express = require('express');
-
 const app = express();
+const querystring = require('querystring');
+const port = 8881;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+// app.METHOD (PATH, HANDLER);
 
 app.get('/', (req, res) => {
-  res.send('Hello, Dude!');
+  const data = {
+    name: "Matt",
+    isAwesome: true
+  };
+
+  res.json(data);
 });
 
-const port = 8888;
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state'
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email'
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope
+  })
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/callback', (req, res) => {
+  res.send('callback');
+})
+
 app.listen(port, () => {
   console.log(`Express app listening at http:localhost:${port}`);
-})
+});
