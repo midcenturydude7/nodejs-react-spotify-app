7eea4ee41ddb53a0409a1b7dd3d57635ab4e9ba0
midCenturyDude7 <mgriffes@gmail.com>
2022-04-08 18:54:46 -0700

    Setting routing with React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { accessToken, logout } from './spotify';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 import './App.css';
 
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = React.useState(null);
+  const [profile, setProfile] = React.useState(null);
 
   React.useEffect(() => {
     setToken(accessToken);
+    
+    const fetchData = async () => {
+      try {
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+
+    catchErrors(fetchData());
 
   }, []);
   
@@ -19,10 +49,38 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <h1>Logged in!</h1>
-            <button onClick={logout}>Log Out</button>
-          </>
+          <Router>
+            <ScrollToTop />
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
+
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header> 
     </div>
