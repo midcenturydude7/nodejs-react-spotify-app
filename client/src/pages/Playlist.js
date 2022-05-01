af02995cf6cccebcbf58a53f802df6c0abb0e527
midCenturyDude7 <mgriffes@gmail.com>
2022-05-01 10:50:22 -0700

    Updated with getPlaylistById fn

---
+++ client/src/pages/Playlist.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { getPlaylistById } from '../spotify';
+import { catchErrors } from '../utils';
+import { StyledHeader } from '../styles';
+
+const Playlist = () => {
+  const { id } = useParams();
+  const [playlist, setPLaylist] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getPlaylistById(id);
+      setPLaylist(data);
+    };
+
+    catchErrors(fetchData());
+  }, [id]);
+  
+  return (
+    <>
+    {playlist && (
+      <>
+        <StyledHeader>
+          <div className="header__inner">
+            {playlist.images.length && playlist.images[0].url && (
+              <img className="header__img" src={playlist.images[0].url} alt="Playlist Artwork"/>
+            )}
+            <div>
+              <div className="header__overline">Playlist</div>
+              <h1 className="header__name">{playlist.name}</h1>
+              <p className="header__meta">
+                {playlist.followers.total ? (
+                  <span>{playlist.followers.total} {`follower${playlist.followers.total !== 1 ? 's' : ''}`}</span>
+                ) : null}
+                <span>{playlist.tracks.total} {`song${playlist.tracks.total !== 1 ? 's' : ''}`}</span>
+              </p>
+            </div>
+          </div>
+        </StyledHeader>
+      </>
+    )}
+  </>
+  );
+};
+
+export default Playlist;
